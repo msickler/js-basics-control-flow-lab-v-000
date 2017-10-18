b9fc6af6c2917b47b4dc5a1870f1262bb7e5ed3c
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-18 17:52:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 // Write your code in this file!
-scuberGreetingForFeet()
+function scuberGreetingForFeet(ride) {
+  if (ride <= 400) {
+    'This one is on me!';
+  } else if (ride >= 2000 && ride <= 2500) {
+    'I will gladly take your thirty bucks.'
+  } else {
+    'No can do.'
+  }
+}
